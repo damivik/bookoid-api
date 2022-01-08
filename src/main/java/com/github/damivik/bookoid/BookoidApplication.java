28927c4db802ebabb0907ccf57348860841c469a
Dami Oyediran <oyediran.viktor@gmail.com>
2022-01-08 16:44:07 +0100

    Initial commit

---
+++ src/main/java/com/github/damivik/bookoid/BookoidApplication.java
@@ -0,0 +1,13 @@
+package com.github.damivik.bookoid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookoidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookoidApplication.class, args);
+	}
+
+}
